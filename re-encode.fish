2ba47b453d151674ebd68bca79fef8a859b1140b
Elec3137 <elec3137@proton.me>
2025-08-16 11:41:42 -0700

    rework encoding script for clarity

--- re-encode.fish
+++ re-encode.fish
@@ -1,79 +1,63 @@
 #!/usr/bin/env fish
-set output_dir './re-encoded'
+
+function print_usage
+    printf 'usage: <input_dir> [<regexp>]
+
+re-encodes (videos) from given $input_dir to its parent directory
+'
+end
+
+function err -d 'prints the given error message and exits'
+    set_color brred >&2
+    printf "%s\n" "$argv" >&2
+    set_color normal >&2
+    exit 1
+end
+
+if test (count $argv) -eq 0
+    print_usage
+    exit
+end
+
+# handle params
+
 set input_dir $argv[1]
-set regexp $argv[2]
-
-# handle incorrect inputs
 if not path is -d "$input_dir"
-	echo "'$input_dir' is not a directory! Exiting"
-	exit 1
+    err "'$input_dir' is not a valid path"
 end
 
-function err
-	set_color brred
-	echo $argv
-	set_color normal
+set output_dir $(path dirname "$input_dir")
+if test "$(path normalize "$input_dir")" -eq "$(path normalize "$output_dir")"
+    err "output dir '$output_dir' should not be equal to input dir '$input_dir' for risk of self-recursion"
 end
 
-# returns true if $regexp is empty or if it matches with any input arg
-function matches_regexp
-	if [ -z "$regexp" ]
-		return 0
-	end
-	
-	for input in $argv
-
-		if [ -n "$input" ] && string match -qr "$regexp" "$input" 
-			return 0
-		end
-	end
-
-	return 1
+if test -n "$argv[2]"
+    set -g regexp "$argv[2]"
+else
+    set -g regexp "."
 end
 
-# find wrappers for readability
-function dirs_at
-	find $argv -type d
-end
-function files_at
-	find $argv -type f
-end
+# begin logic
 
-# create the local output directory
-mkdir -pv "$output_dir" || exit
-
-# iterate through each directory in the input
-for dir in $(dirs_at "$input_dir")
-
-	# mkdir if there are regexp matches in this directory
-	if matches_regexp $(files_at "$dir")
-
-		# this may make directories even if there are no media files in them
-		# however there is not much of a choice; `file` is not so helpful either
-		# see: https://stackoverflow.com/questions/8101812/using-the-linux-file-command-to-determine-type-ie-image-audio-or-video
-		mkdir -pv "$output_dir/$dir" || exit 
-	end
+if not mkdir -pv "$output_dir"
+    err "unable to create output directory"
 end
 
 # iterate through each file of the source directory
-for file in $(files_at "$input_dir")
+for file in $(fd "$regexp" -t file "$input_dir")
+    if not mkdir -pv "$output_dir/$(path dirname "$file")"
+        err "unable to create output subdirectory"
+    end
 
-	if matches_regexp "$file"
-		set output_file "$output_dir/$(path change-extension 'mkv' "$file")"
+    set output_file "$output_dir/$(path change-extension 'mkv' "$file")"
 
-		if path is $output_file
-			err "Output file '$output_file' already exists, SKIPPING"
-			continue
-		end
+    if path is "$output_file"
+        err "output file '$output_file' already exists!"
+    end
 
-		# re-encode the video to the target; if this doesn't work trash the result
-		if not ffmpeg -i "$file" -pix_fmt yuv420p10le -c:v libsvtav1 -crf 35 -svtav1-params tune=0:keyint=10s -preset 0 -c:a libopus -b:a 128k -c:s copy "$output_file"
-			if trash "$output_file"
-				err "Failed to encode to '$output_file', output is TRASHED"
-			else
-				err "Failed to encode to '$output_file', EXITING"
-				exit 1
-			end
-		end
-	end
+    # re-encode the video to the output; if this doesn't work trash the result and exit
+    if not ffmpeg -i "$file" -pix_fmt yuv420p10le -c:v libsvtav1 -crf 35 -svtav1-params tune=0:keyint=10s -preset 0 -c:a libopus -b:a 128k -c:s copy "$output_file"
+        trash "$output_file"
+        err "failed to encode to '$output_file'!"
+    end
 end
