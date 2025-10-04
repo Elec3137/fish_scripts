74184eaeb13d23330835e9cde2d3a7a692ac411b
Elec3137 <elec3137@proton.me>
2025-10-04 13:43:20 -0700

    improve error/info messages

--- jxl.fish
+++ jxl.fish
@@ -2,23 +2,41 @@
 # This is a script to re-encode all jpegs and pngs into JXLs (losslessly)
 # JXL is a more efficient format, therefore this saves space.
 
+function err -d 'prints the given error message and exits'
+    set_color brred >&2
+    printf "%s\n" "$argv" >&2
+    set_color normal >&2
+end
+
+function info -d 'prints info as cyan into stderr, to enhance readability and avoid being hidden and used by redirection'
+    set_color brcyan >&2
+    printf "%s\n" "$argv" >&2
+    set_color normal >&2
+end
+
 for file in $(fd . ./ --type file)
-	if string match -qr "jpeg|jpg|png" "$(string lower $(path extension "$file"))"
-		printf "\tProcessing '%s'...\n" "$file"
+    if string match -qr "jpeg|jpg|png" "$(string lower $(path extension "$file"))"
+        info "Processing '$file'..."
 
-		set newfile $(path change-extension 'jxl' "$file")
+        set newfile $(path change-extension 'jxl' "$file")
 
-		# If the target file doesn't already exist, and the command works properly
-		if not path is "$newfile" && cjxl --lossless_jpeg=1 -q 100 -e 10 "$file" "$newfile"
-			trash "$file" || exit
-			set_color cyan; printf "\tTrashed '%s', wrote '%s'\n" "$file" "$newfile"; set_color normal
-		else
-			set_color red; printf "\tFailed to process '%s'\n" "$file"; set_color normal
-		end
+        if path is "$newfile"
+            err "Output file '$newfile' already exists, skipping"
+        else if cjxl --lossless_jpeg=1 -q 100 -e 10 "$file" "$newfile"
+            info "$(du -h "$file") -> $(du -h "$newfile")"
+            if trash "$file"
+                info "Trashed '$file', wrote '$newfile'"
+            else
+                err "Failed to trash '$file'"
+                exit 1
+            end
+        else
+            err "Failed to process '$file'"
+        end
 
-	else 
-		printf "\tSkipped '%s'\n" "$file"
-	end
+    else
+        info "Skipped '$file'"
+    end
 
-	printf "\n"
+    info
 end
