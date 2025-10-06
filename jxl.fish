a502e851e4032780252923c7d6c2d6fa7517bd1c
Elec3137 <elec3137@proton.me>
2025-10-06 09:56:27 -0700

    manually use a trash dir for compatibility
    also always exit on err
    and omit filenames when showing change in filesize

--- jxl.fish
+++ jxl.fish
@@ -2,10 +2,13 @@
 # This is a script to re-encode all jpegs and pngs into JXLs (losslessly)
 # JXL is a more efficient format, therefore this saves space.
 
+set trashdir "$(pwd)/original-images_$(date -I)"
+
 function err -d 'prints the given error message and exits'
     set_color brred >&2
     printf "%s\n" "$argv" >&2
     set_color normal >&2
+    exit 1
 end
 
 function info -d 'prints info as cyan into stderr, to enhance readability and avoid being hidden and used by redirection'
@@ -14,6 +17,14 @@ function info -d 'prints info as cyan into stderr, to enhance readability and av
     set_color normal >&2
 end
 
+info "making trash directory at '$trashdir'"
+if not mkdir "$trashdir"
+    if path is -f "$trashdir"
+        err "trash directory '$trashdir' is a file, cannot continue"
+    end
+end
+info
+
 for file in $(fd . ./ --type file)
     if string match -qr "jpeg|jpg|png" "$(string lower $(path extension "$file"))"
         info "Processing '$file'..."
@@ -21,14 +32,11 @@ for file in $(fd . ./ --type file)
         set newfile $(path change-extension 'jxl' "$file")
 
         if path is "$newfile"
-            err "Output file '$newfile' already exists, skipping"
+            err "Output file '$newfile' already exists"
         else if cjxl --lossless_jpeg=1 -q 100 -e 10 "$file" "$newfile"
-            info "$(du -h "$file") -> $(du -h "$newfile")"
-            if trash "$file"
-                info "Trashed '$file', wrote '$newfile'"
-            else
+            info "$(du -h "$file" | cut -f 1) -> $(du -h "$newfile" | cut -f 1)"
+            if not mv -vt "$trashdir" "$file"
                 err "Failed to trash '$file'"
-                exit 1
             end
         else
             err "Failed to process '$file'"
@@ -39,4 +47,11 @@ for file in $(fd . ./ --type file)
     end
 
     info
+end
+
+read -P "Delete original images in '$trashdir' now? [y/N] " input
+set input (string lower -- $input)
+
+if test "$input" = y
+    rm -rv "$trashdir"
 end
